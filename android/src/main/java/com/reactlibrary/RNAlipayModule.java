14693f35a837493825a8b5cfbf2e44be6bbc75f5
xy113 <songdewei@163.com>
2020-12-23 05:14:47 +0800

    111

--- android/src/main/java/com/reactlibrary/RNAlipayModule.java
+++ android/src/main/java/com/reactlibrary/RNAlipayModule.java
@@ -1,10 +1,19 @@
 
 package com.reactlibrary;
 
+import com.alipay.sdk.app.EnvUtils;
+import com.alipay.sdk.app.H5PayCallback;
+import com.alipay.sdk.util.H5PayResultModel;
+import com.alipay.sdk.app.AuthTask;
+import com.alipay.sdk.app.PayTask;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+import java.util.Map;
 
 public class RNAlipayModule extends ReactContextBaseJavaModule {
 
